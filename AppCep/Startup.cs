d2df6cc23f9457c19b076a66274d677f4ca7aa7c
Aluno <Aluno@DOSO129419>
2019-07-06 15:09:08 -0300

    Commit - Identação

--- AppCep/Startup.cs
+++ AppCep/Startup.cs
@@ -39,6 +39,7 @@ namespace AppCep
             
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().AddJsonOptions(options => options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
